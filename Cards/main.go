9417d5293658a24a1951c87681f3aa3e1a20f4dd
Felipe Espinoza B <zoek@zoeks-MacBook-Pro.local>
2022-07-20 21:36:53 -0400

    feat: save deck to file in our hard drive

--- Cards/main.go
+++ Cards/main.go
@@ -1,12 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
-
-	fmt.Println(hand)
-	fmt.Println(remainingCards)
+	cards.saveToFile("my_cards")
 }
