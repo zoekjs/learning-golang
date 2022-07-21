90170e8fa8dc712c63f6864ee74948552e7ed629
zoekjs <felipe.esbu@gmail.com>
2022-07-20 22:11:25 -0400

    feat: load deck from file in our hard drive

--- Cards/deck.go
+++ Cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -37,6 +38,20 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
+//save deck to file in our hard drive
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
 }
